791934d87c7eeafa77e3bf46340bbf1aa745f742
KritimPrasadKafle <kritim10k@gmail.com>
2024-08-24 23:34:49 +0530

    24082024

---
+++ Web3.0/Week2/script.js
@@ -0,0 +1,20 @@
+let str = "h";
+const binaryRepresentation = new TextEncoder().encode(str);
+
+let d = new Date();
+console.log(binaryRepresentation)
+
+
+const binaryRepresentation2 = [104];
+
+const arr = new Uint8Array([1919]);
+console.log(arr)
+
+const publickey = "kritim";
+const bytes = new TextEncoder().encode(publickey);
+console.log(bytes);
+
+
+
+
+
