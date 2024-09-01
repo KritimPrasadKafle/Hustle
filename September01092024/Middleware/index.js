19b23ab8b8a63b78e4f6d55f9d70a6da296a68d8
KritimPrasadKafle <kritim10k@gmail.com>
2024-09-01 23:21:39 +0530

    09September2024

---
+++ September01092024/Middleware/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+
+const app = express();
+
+function isOldEnough(age) {
+  if (age >= 14) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughMiddleware(req, res, next) {
+  if (age >= 14) {
+    next();
+
+  } else {
+    res.json({
+      msg: "Sorry You are nt of age yet",
+    })
+  }
+}
+
+app.get("/ride1", function (req, res) {
+  if (isOldEnough(req.query.age)) {
+    res.json({
+      msg: "You have successfully riden the ride 1"
+    })
+  }
+  else {
+    res.status(411).json({
+      msg: "Sorry you are not of age yet"
+    })
+  }
+
+}
+)
+
+
+
+
+app.get("/ride2", function (req, res) {
+  if (isOldEnough(req.query.age)) {
+    res.json({
+      msg: "You have successfully riden the ride 1"
+    })
+  }
+  else {
+    res.status(411).json({
+      msg: "Sorry you are not of age yet"
+    })
+  }
+
+}
+)
+
+app.listen(4001);
