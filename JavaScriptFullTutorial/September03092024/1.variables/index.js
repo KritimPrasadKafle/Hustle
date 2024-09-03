43a31acaf411448e0d6644aee793872d81bcb896
KritimPrasadKafle <kritim10k@gmail.com>
2024-09-04 00:16:45 +0530

    September042024

---
+++ JavaScriptFullTutorial/September03092024/1.variables/index.js
@@ -0,0 +1,14 @@
+const accountId = 144553;
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2  //not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "6789"
+accountCity = "Bangalore"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountCity, accountState]);
---
+++ JavaScriptFullTutorial/September03092024/2.Datatype and ECMA standards/index.js
@@ -0,0 +1,23 @@
+"use strict" //treat all JS code for newer version
+
+// alert(2+3);
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+//object 
+
+console.log(typeof "kritim");
+
+
