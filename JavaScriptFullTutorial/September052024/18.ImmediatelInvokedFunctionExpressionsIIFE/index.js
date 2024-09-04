b0442466ebeea47c715ffa5de5dbcc416bdb8734
KritimPrasadKafle <kritim10k@gmail.com>
2024-09-05 00:52:14 +0530

    IIFE

---
+++ JavaScriptFullTutorial/September052024/18.ImmediatelInvokedFunctionExpressionsIIFE/index.js
@@ -0,0 +1,16 @@
+(function chai() {
+  console.log(`DB Connected`);
+
+})();
+
+(() => {
+  console.log(`DB CONNECTED TWO`);
+})();
+
+(function aurcode() {
+  console.log(`DB CONNECTED TWO`);
+})();
+
+((name) => {
+  console.log(`DB CONNECTED TWO ${name}`);
+})("Kritim");
