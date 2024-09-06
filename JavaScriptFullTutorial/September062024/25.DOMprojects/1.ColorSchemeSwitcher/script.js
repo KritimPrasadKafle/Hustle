294d08021ae853f4c48efedfc4f7913f3e1300fd
KritimPrasadKafle <kritim10k@gmail.com>
2024-09-06 21:59:43 +0530

    ColorChanger

---
+++ JavaScriptFullTutorial/September062024/25.DOMprojects/1.ColorSchemeSwitcher/script.js
@@ -0,0 +1,34 @@
+const btns = document.querySelectorAll('.button');
+
+const body = document.querySelector("body");
+
+btns.forEach(function (button) {
+  // console.log(button);
+
+  button.addEventListener('click', function (e) {
+    // console.log(e);
+    console.log(e.target);
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'white') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'black') {
+      body.style.backgroundColor = e.target.id;
+      body.style.color = 'white';
+    }
+
+
+
+  })
+
+})
+
+// console.log(btns);
