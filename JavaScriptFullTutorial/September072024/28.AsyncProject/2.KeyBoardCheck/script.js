a2764acb272388b96c81a794b089e750b49fc029
KritimPrasadKafle <kritim10k@gmail.com>
2024-09-07 14:19:26 +0530

    ASync

---
+++ JavaScriptFullTutorial/September072024/28.AsyncProject/2.KeyBoardCheck/script.js
@@ -0,0 +1,22 @@
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (e) => {
+  insert.innerHTML = `<div class = 'color'>
+  <table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>${e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+</table></div>`
+})
