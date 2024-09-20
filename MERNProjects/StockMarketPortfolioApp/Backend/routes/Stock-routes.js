38aec79bb8e1fbcd5c58a433984ba634d3f94a50
KritimPrasadKafle <kritim10k@gmail.com>
2024-09-21 00:30:42 +0530

    SimpleSocialMedia

--- MERNProjects/StockMarketPortfolioApp/Backend/routes/Stock-routes.js
+++ MERNProjects/StockMarketPortfolioApp/Backend/routes/Stock-routes.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb+srv://admin:1234567890@cluster0.jyrg0.mongodb.net/StockPortfolioApp", {
+mongoose.connect("", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
