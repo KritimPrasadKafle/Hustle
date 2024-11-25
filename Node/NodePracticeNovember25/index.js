0d0386c207c8d9abfd4009ffb0fde3dcbca3f925
KritimPrasadKafle <kritim10k@gmail.com>
2024-11-26 00:23:33 +0545

    FileSystem

--- Node/NodePracticeNovember25/index.js
+++ Node/NodePracticeNovember25/index.js
@@ -1,3 +1,25 @@
 const fs = require('fs');
 
-fs.writeFileSync('./test.txt', "Hey there");
+//sync
+// fs.writeFileSync('./test.txt', "Hey there");
+
+//async
+// fs.writeFile("./test.txt", "Hello World Async", (err) => {})
+
+// const content = fs.readFileSync("./contacts.txt", "utf-8");
+// console.log(content);
+
+const content1 = fs.readFile("./contacts.txt", "utf-8", (err, result) => {
+
+  if (err) {
+    console.log(err);
+  }
+  else {
+    console.log(result);
+  }
+});
+
+
+
+
+
---
+++ Node/NodePracticeNovember25/trymodule.js
@@ -0,0 +1,7 @@
+const { subtract, add } = require('./math.js');
+const ans = add(4, 5);
+console.log(ans);
+
+const ans1 = subtract(6, 2);
+console.log(ans1);
+
