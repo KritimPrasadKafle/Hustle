cbd2b20a69ab9158fefc6c1836617a3ac74ea756
KritimPrasadKafle <kritim10k@gmail.com>
2024-11-26 00:59:00 +0545

    Nonblocking and Blocking code

--- Node/NodePracticeNovember25/index.js
+++ Node/NodePracticeNovember25/index.js
@@ -9,15 +9,53 @@ const fs = require('fs');
 // const content = fs.readFileSync("./contacts.txt", "utf-8");
 // console.log(content);
 
-const content1 = fs.readFile("./contacts.txt", "utf-8", (err, result) => {
+// const content1 = fs.readFile("./contacts.txt", "utf-8", (err, result) => {
 
-  if (err) {
-    console.log(err);
-  }
-  else {
-    console.log(result);
-  }
+//   if (err) {
+//     console.log(err);
+//   }
+//   else {
+//     // console.log(result);
+//   }
+// });
+
+
+// fs.appendFileSync("./test.txt", new Date().toISOString());
+
+// fs.unlinkSync("./test.txt");
+// fs.mkdirSync("my-docs");
+
+
+
+
+// sync ... Blocking
+// fs.writeFileSync('./test.txt', "Hey there");
+
+// async ... Non-Blocking
+// fs.writeFile("./test.txt", "Hello World Async", (err) => { })
+
+
+// Blocking code
+
+// console.log("1");
+
+// const result = fs.readFileSync('contacts.txt', "utf-8");
+
+// console.log(result);
+// console.log("2");
+
+
+const os = require('os');
+console.log(os.cpus().length);
+console.log("1");
+
+const result1 = fs.readFile('contacts.txt', "utf-8", (err, res) => {
+  console.log(res);
+
 });
+
+
+console.log("2");
 
 
 
