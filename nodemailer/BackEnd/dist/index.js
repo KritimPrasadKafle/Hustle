01322d0eadbb26b9655e601586b07ca5acd8e811
KritimPrasadKafle <kritim10k@gmail.com>
2025-01-04 20:07:10 +0545

    nodemailer

--- nodemailer/BackEnd/dist/index.js
+++ nodemailer/BackEnd/dist/index.js
@@ -1,14 +1,55 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const nodemailer_1 = __importDefault(require("nodemailer"));
+require("dotenv").config();
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
 const port = 3000;
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+app.post("/deliver", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { recipient, subject, text, html } = req.body;
+    try {
+        const transporter = nodemailer_1.default.createTransport({
+            service: "gmail",
+            host: "smtp.gmail.com",
+            secure: true,
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PASSWORD,
+            },
+        });
+        const info = yield transporter.sendMail({
+            from: process.env.EMAIL,
+            to: recipient,
+            subject: subject,
+            text: text,
+            html: html,
+        });
+        console.log("Message sent: %s", info.messageId);
+        res.status(200).send("Message sent");
+    }
+    catch (error) {
+        console.error("Error sending email:", error);
+        res.status(500).send("Error sending email");
+    }
+}));
 app.listen(port, () => {
     console.log(`app running in ${port}`);
 });
--- nodemailer/BackEnd/src/index.ts
+++ nodemailer/BackEnd/src/index.ts
@@ -1,13 +1,48 @@
 import express, { Request, Response } from "express";
+import nodemailer from "nodemailer";
+
+require("dotenv").config();
+import bodyParser from "body-parser";
 
 const app = express();
 
 const port = 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
+app.post("/deliver", async (req: Request, res: Response) => {
+  const { recipient, subject, text, html } = req.body;
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      secure: true,
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL,
+      to: recipient,
+      subject: subject,
+      text: text,
+      html: html,
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    res.status(200).send("Message sent");
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).send("Error sending email");
+  }
+});
 app.listen(port, () => {
   console.log(`app running in ${port}`);
 });
