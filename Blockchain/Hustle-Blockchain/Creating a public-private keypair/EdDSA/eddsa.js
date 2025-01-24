70f3e5d3b7e041e6f2c6f8899d4a76206117b7c2
KritimPrasadKafle <kritim10k@gmail.com>
2025-01-24 22:27:57 +0545

    Generate public and private key using solana web3

--- Blockchain/Hustle-Blockchain/Creating a public-private keypair/EdDSA/eddsa.js
+++ Blockchain/Hustle-Blockchain/Creating a public-private keypair/EdDSA/eddsa.js
@@ -3,17 +3,23 @@ async function main() {
   //Generate a secure random private key
   const privateKey = ed.utils.randomPrivateKey();
 
+
   //Convert the message "hello world to a Uint8Array"
   const message = new TextEncoder().encode("hello world");
+
 
   //Generate the public key from the private key
   const publicKey = await ed.getPublicKeyAsync(privateKey);
 
+
+
   //Sign the message
   const signature = await ed.signAsync(message, privateKey);
 
+
   //Verify the signature
   const isValid = await ed.verifyAsync(signature, message, publicKey);
+
 
   //output the result
   console.log(isValid);
---
+++ Blockchain/Hustle-Blockchain/Creating a public-private keypair/solanaweb3/index.js
@@ -0,0 +1,21 @@
+import { Keypair } from "@solana/web3.js";
+import nacl from "tweetnacl";
+
+//Generate a new keypair
+const keypair = Keypair.generate();
+
+const publicKey = keypair.publicKey.toString();
+const secretKey = keypair.secretKey;
+
+//Display the keys
+console.log("Public key:", publicKey);
+console.log("Private key:", secretKey);
+
+//Convert the message "hello world" to a Uint8Array
+const message = new TextEncoder().encode("hello world");
+
+const signature = nacl.sign.detached(message, secretKey);
+const result = nacl.sign.detached.verify(message, signature, keypair.publicKey.toBytes(),);
+console.log(result);
+
+
