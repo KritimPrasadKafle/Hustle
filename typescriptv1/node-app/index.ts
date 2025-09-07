de5fa31915c80596fe948e678bf37df6377bef53
dbipin <bipin.dhimal1@gmail.com>
2025-09-07 23:10:22 +0545

    Typescript

--- typescriptv1/node-app/index.ts
+++ typescriptv1/node-app/index.ts
@@ -36,3 +36,51 @@ one(function(){
     console.log("hi there");
     
 })
+
+const user = {
+    firstName: 'kritim',
+    lastName: 'kafle',
+    email: 'hello@gmail.com',
+    age: 21
+}
+
+interface User{
+    firstName: string;
+    lastName: string;
+    email: string;
+    age: number;
+}
+
+
+function isLegalv1(user: User): Boolean{
+    if (user.age > 18){
+        return true;
+        
+    }else{
+        return false;
+    }
+}
+
+
+
+isLegalv1(user);
+
+// Todo.tsx
+interface TodoType {
+  title: string;
+  description: string;
+  done: boolean;
+}
+
+interface TodoInput {
+  todo: TodoType;
+}
+
+// function Todo({ todo }: TodoInput) {
+//   return <div>
+//     <h1>{todo.title}</h1>
+//     <h2>{todo.description}</h2>
+    
+//   </div>
+// }
+
